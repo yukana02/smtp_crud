fbb501bb3797b8d58e3642b95d6cd793b4e461e9
yukana02 <yudafbs02@gmail.com>
2024-02-12 01:09:33 +0700

    update searcing controller

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Crud;
+use App\Models\User;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -11,16 +12,21 @@ class DashController extends Controller
 {
     
     public function index(Request $request): View
-    {
-        $query = DB::table('cruds')
-            ->join('users', 'users.id', '=', 'cruds.iduser')
-            ->select('cruds.title','cruds.id')
-            ->latest('cruds.created_at');
-    
-        $data = $request->has('cari') ? $query->where('title', 'like', '%' . $request->input('cari') . '%')->paginate(10) : $query->paginate(10);
-    
-        return view('dashboard.dash-user', compact('data'));
+{
+    $query = DB::table('cruds')
+        ->join('users', 'users.id', '=', 'cruds.iduser')
+        ->select('cruds.title','cruds.id')
+        ->latest('cruds.created_at');
+
+    if ($request->has('cari')) {
+        $data = $query->where('title', 'like', '%' . $request->input('cari') . '%')->paginate(10);
+    } else {
+        $data = $query->paginate(10);
     }
+
+    return view('dashboard.dash-user', compact('data'));
+}
+
     
 
 }
